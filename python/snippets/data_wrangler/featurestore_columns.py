ebdc09dd58a9e8afb43dd71530529ed13041056a
Chae Clark <37343485+chaeAclark@users.noreply.github.com>
2022-12-20 09:32:41 -0500

    Create featurestore_columns.py

---
+++ python/snippets/data_wrangler/featurestore_columns.py
@@ -0,0 +1,6 @@
+from time import time
+
+# I don't generally like use fractional seconds, but you're free to
+# by removing the int() cast
+df['EventTime'] = float(int(time()))
+df['RecordID'] = df.reset_index(drop=True).index.astype(str) + '_' + df['EventTime']
