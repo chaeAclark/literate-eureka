de6b1aca5299d2d3adf58b08c796643dc285d9a6
Chae Clark <37343485+chaeAclark@users.noreply.github.com>
2022-12-20 09:34:02 -0500

    Update featurestore_columns.py

--- python/snippets/data_wrangler/featurestore_columns.py
+++ python/snippets/data_wrangler/featurestore_columns.py
@@ -3,4 +3,4 @@ from time import time
 # I don't generally like use fractional seconds, but you're free to
 # by removing the int() cast
 df['EventTime'] = float(int(time()))
-df['RecordID'] = df.reset_index(drop=True).index.astype(str) + '_' + df['EventTime']
+df['RecordID'] = df.reset_index(drop=True).index.astype(str) + '_' + df['EventTime'].astype(str)
